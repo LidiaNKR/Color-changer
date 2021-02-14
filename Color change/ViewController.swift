9b4d12c1c102354826f03800c7917fdd48832981
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2021-02-14 12:40:49 +0300

    MiddleSetup

--- Color change/ViewController.swift
+++ Color change/ViewController.swift
@@ -10,31 +10,68 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var mainLabel: UIView!
+   
+    @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
-    @IBOutlet var redSlider: UISlider!
-    @IBOutlet var greenSliderValue: UILabel!
-    @IBOutlet var blueSliderValue: UILabel!
-    @IBOutlet var redSliderValue: UILabel!
+
+    @IBOutlet var redSliderValueLabel: UILabel!
+    @IBOutlet var greenSliderValueLabel: UILabel!
+    @IBOutlet var blueSliderValueLabel: UILabel!
+    
+    @IBOutlet var redColorValueTextField: UITextField!
+    @IBOutlet var greenColorValueTextField: UITextField!
+    @IBOutlet var blueColorValueTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    func changerColor() {
-        mainLabel.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
-    }
-    
-    @IBAction func sliderChsnger() {
-    greenSliderValue.text = String(format: "%.2f",
-                                       greenSlider.value)
-        blueSliderValue.text = String(format: "%.2f",
-                                      blueSlider.value)
-        redSliderValue.text = String(format: "%.2f",
-                                     redSlider.value)
-    }
-    
-    @IBAction func colorChanger() {
+        mainLabel.layer.cornerRadius = 10
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
         changerColor()
+        
+        setValue(for: redSliderValueLabel, greenSliderValueLabel, blueSliderValueLabel)
+    }
+    
+    @IBAction func sliderChanger(_ sender: UISlider) {
+        changerColor()
+        
+        switch sender.tag {
+        case 0: redSliderValueLabel.text = string(from: sender)
+        case 1: greenSliderValueLabel.text = string(from: sender)
+        case 2: blueSliderValueLabel.text = string(from: sender)
+        default: break
+        }
+        
+    }
+    
+    
+    @IBAction func doneBittonPressed() {
+        dismiss(animated: true)
+    }
+    
+    private func changerColor() {
+        mainLabel.backgroundColor = UIColor(red:CGFloat(redSlider.value),
+                                            green:CGFloat(greenSlider.value),
+                                            blue:CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redSliderValueLabel.text = string(from: redSlider)
+            case 1: greenSliderValueLabel.text = string(from: greenSlider)
+            case 2: blueSliderValueLabel.text = string(from: blueSlider)
+            default: break
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
