73d88987023487547898f98ea4a52a3f4177bde3
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2021-02-15 03:22:59 +0300

    MiddleSetup

--- Color change/ColorBackgroundViewController.swift
+++ Color change/ColorBackgroundViewController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class ColorBackgroundViewController: UIViewController {
+    
+    private var backGrounColorMainView: UIColor
+    var deligate: UITextFieldDelegate!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let navigationVC = segue.destination as? UINavigationController else { return }
+        guard let settingsVC = navigationVC.topViewController as? SettingsViewController  else { return }
+        settingsVC.backGrounColorMainView = backGrounColorMainView
     }
-    */
+    
+
+
+
 
 }
+//
+//extension ColorBackgroundViewController {
+//    func setNewValues(for backGrounColorMainView: backGrounColorMainView) {
+//        UIColor(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: 1)
+//
+//    }
+//}
---
+++ Color change/RGBValues.swift
@@ -0,0 +1,14 @@
+//
+//  RGBValues.swift
+//  Color change
+//
+//  Created by Лидия Ладанюк on 15.02.2021.
+//
+
+import Foundation
+
+struct GGBValue {
+    var red: Double
+    var green: Double
+    var blue: Double
+}
